2905b6fe823b5f2b18e700bc15d92e1bd71e2b49
Tocno NeAleks <nealeks18@mail.ru>
2024-03-26 19:13:06 +0900

    Init

---
+++ src/Console.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+
+namespace src.kernel.consolesys
+{
+    public class ConsoleSYS
+    {
+        private static ConsoleColor DefaultTextColor = ConsoleColor.White;
+
+        public static void Write(string output, bool newline, short color = 0)
+        {
+            // NOT WORKING: Encoding not change. Cyrillic not supported.
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Setting Color
+            switch (color)
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+
+                case 5:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+
+                case 6:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+
+                case 7:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+
+                case 8:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+
+            // Checking New Line?
+            if (newline) Console.WriteLine(output);
+            else Console.Write(output);
+
+            // Set Color to Default
+            if (color != 0) Console.ForegroundColor = DefaultTextColor;
+        }
+
+        public static void Execute(string fullcmd)
+        {
+            // Remove the arguments from the cmd
+            var cmd = fullcmd.Split(' ')[0];
+
+            // Remove the command from the list of arguments
+            var arguments = fullcmd.Split(' ').Skip(1).ToArray();
+
+            // Command Handler
+            switch (cmd)
+            {
+                // CORE !! DON'T TOUCH THIS !!
+                case "kernel":
+                    if (arguments.Length > 0)
+                    {
+                        if (arguments.Contains("-v") || arguments.Contains("--version")) Write($"Version Kernel: {Kernel.Version}", true, 3);
+                        else if (arguments.Contains("-i")) Write($"CPU: {Cosmos.Core.CPU.GetCPUBrandString()}", true);
+                        else if (arguments.Contains("-s") || arguments.Contains("--shutdown")) Cosmos.System.Power.Shutdown();
+                        else Write("using: kernel [option] \nexample: kernel -v", true, 3);
+                    }
+                    else
+                    {
+                        Write("using: kernel [option] \nexample: kernel -v", true, 3);
+                    }
+                    break;
+
+                // Clearing Console
+                case "clr":
+                    Console.Clear();
+                    break;
+
+                case "clear":
+                    Console.Clear();
+                    break;
+
+                // When not found command
+                default:
+                    Write(fullcmd + ": Unknown command. Please contact us.", true, 4);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Kernel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using src.kernel.consolesys;
+
+namespace src.kernel
+{
+    public class Kernel : Sys.Kernel
+    {
+        public static readonly string Version = "0.1";
+
+
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to C# OS by everyofflineuser version 0.1");
+        }
+
+        protected override void Run()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            ConsoleSYS.Write("$Kernel SHELL ", false, 3);
+            ConsoleSYS.Write(">> ", false, 7);
+            var cmd = Console.ReadLine();
+
+            ConsoleSYS.Execute(cmd);
+        }
+    }
+}
